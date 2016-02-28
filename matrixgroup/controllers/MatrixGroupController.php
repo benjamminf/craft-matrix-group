ee63974750b59a8501d53b47a8836538d40f0f40
Benjamin Fleming <benjfleming21@gmail.com>
2016-02-28 19:45:08 +1100

    Group status now saved into DB

--- matrixgroup/controllers/MatrixGroupController.php
+++ matrixgroup/controllers/MatrixGroupController.php
@@ -3,10 +3,92 @@ namespace Craft;
 
 class MatrixGroupController extends BaseController
 {
+	/**
+	 * Copied from Craft\FieldsController::actionSaveField()
+	 * @throws HttpException
+	 */
 	public function actionSaveField()
 	{
 		$this->requirePostRequest();
 
-		craft()->runController('fields/saveField');
+		$field = new FieldModel();
+
+		$field->id           = craft()->request->getPost('fieldId');
+		$field->groupId      = craft()->request->getRequiredPost('group');
+		$field->name         = craft()->request->getPost('name');
+		$field->handle       = craft()->request->getPost('handle');
+		$field->instructions = craft()->request->getPost('instructions');
+		$field->translatable = (bool) craft()->request->getPost('translatable');
+
+		$field->type = craft()->request->getRequiredPost('type');
+
+		$typeSettings = craft()->request->getPost('types');
+		if(isset($typeSettings[$field->type]))
+		{
+			$field->settings = $typeSettings[$field->type];
+		}
+
+		if(craft()->fields->saveField($field))
+		{
+			/**********************************************************************************************************
+			 * <inject>
+			 */
+
+			if($field->type === 'Matrix')
+			{
+				$postSettings = $typeSettings[$field->type];
+				$fieldType = $field->getFieldType();
+				$typeSettings = $fieldType->getSettings();
+				$blockTypes = $typeSettings->getBlockTypes();
+
+				foreach($postSettings['blockTypes'] as $postId => $postBlockType)
+				{
+					$group = $postBlockType['group'] === 'true';
+					$handle = $postBlockType['handle'];
+
+					foreach($blockTypes as $id => $blockType)
+					{
+						if($blockType->handle === $handle)
+						{
+							$groupBlockType = new MatrixGroup_BlockTypeModel();
+							$groupBlockType->typeId = (int) $blockType->id;
+							$groupBlockType->group = $group;
+
+							if($group)
+							{
+								craft()->matrixGroup->saveBlockType($groupBlockType);
+							}
+							else
+							{
+								craft()->matrixGroup->deleteBlockType($groupBlockType);
+							}
+						}
+					}
+				}
+			}
+
+			/**
+			 * </inject>
+			 **********************************************************************************************************/
+
+			craft()->userSession->setNotice(Craft::t('Field saved.'));
+
+			if(isset($_POST['redirect']) && mb_strpos($_POST['redirect'], '{fieldId}') !== false)
+			{
+				craft()->deprecator->log('FieldsController::saveField():fieldId_redirect', 'The {fieldId} token within the ‘redirect’ param on fields/saveField requests has been deprecated. Use {id} instead.');
+				$_POST['redirect'] = str_replace('{fieldId}', '{id}', $_POST['redirect']);
+			}
+
+			$this->redirectToPostedUrl($field);
+		}
+		else
+		{
+			craft()->userSession->setError(Craft::t('Couldn’t save field.'));
+		}
+
+		// Send the field back to the template
+		craft()->urlManager->setRouteVariables(array(
+			'field' => $field
+		));
 	}
 }
---
+++ matrixgroup/services/MatrixGroupService.php
@@ -0,0 +1,112 @@
+<?php
+namespace Craft;
+
+class MatrixGroupService extends BaseApplicationComponent
+{
+	public function saveBlockType(MatrixGroup_BlockTypeModel $model)
+	{
+		$record = null;
+
+		if(is_int($model->id))
+		{
+			$record = MatrixGroup_BlockTypeRecord::model()->findById($model->id);
+
+			if(!$record)
+			{
+				throw new Exception(Craft::t('No matrix group block type exists with the ID “{id}”.', array('id' => $model->id)));
+			}
+		}
+		else
+		{
+			$record = MatrixGroup_BlockTypeRecord::model()->findByAttributes(array('typeId' => $model->typeId));
+
+			if(!$record)
+			{
+				$record = new MatrixGroup_BlockTypeRecord();
+			}
+		}
+
+		$blockType = craft()->matrix->getBlockTypeById($model->typeId);
+
+		if(!$blockType)
+		{
+			throw new Exception(Craft::t('No matrix block type exists with the ID “{id}”.', array('id' => $model->typeId)));
+		}
+
+		$record->typeId = $model->typeId;
+		$record->group = $model->group;
+
+		$record->validate();
+		$model->addErrors($record->getErrors());
+
+		$success = !$model->hasErrors();
+
+		if($success)
+		{
+			// Create transaction only if this isn't apart of an already occurring transaction
+			$transaction = craft()->db->getCurrentTransaction() ? false : craft()->db->beginTransaction();
+
+			try
+			{
+				$record->save(false);
+				$model->id = $record->id;
+
+				if($transaction)
+				{
+					$transaction->commit();
+				}
+			}
+			catch(\Exception $e)
+			{
+				if($transaction)
+				{
+					$transaction->rollback();
+				}
+
+				throw $e;
+			}
+		}
+
+		return $success;
+	}
+
+	public function deleteBlockType(MatrixGroup_BlockTypeModel $model)
+	{
+		$transaction = craft()->db->getCurrentTransaction() === null ? craft()->db->beginTransaction() : null;
+		try
+		{
+			$condition = array();
+
+			if(is_int($model->id))
+			{
+				$condition['id'] = $model->id;
+			}
+			else if(is_int($model->typeId))
+			{
+				$condition['typeId'] = $model->typeId;
+			}
+			else
+			{
+				return false;
+			}
+
+			$affectedRows = craft()->db->createCommand()->delete('matrixgroup_blocktypes', $condition);
+
+			if($transaction !== null)
+			{
+				$transaction->commit();
+			}
+
+			return (bool) $affectedRows;
+		}
+		catch(\Exception $e)
+		{
+			if($transaction !== null)
+			{
+				$transaction->rollback();
+			}
+
+			throw $e;
+		}
+	}
+}
